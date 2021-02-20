f3c57290c785c0ae164eeb746df955c52be7a774
Taras-777 <taracxomun@gmail.com>
2021-02-20 12:54:25 +0200

    NONE

--- Lab1.py
+++ Lab1.py
@@ -1,19 +1,18 @@
 class Camera:
-    name = ""
-    memory = 0
-    zoom = 0
-    number_of_pixels = 0
-    color = ""
-    photo_quality = ""
+    camera = "Camera"
 
-    def __init__(self, name, memory, zoom, number_of_pixels, color, photo_quality):
+    @staticmethod
+    def static():
+        return Camera.camera
+
+    def __init__(self, name ="", memory=0, zoom=0, number_of_pixels=0, color="", photo_quality=""):
         self.name = name
         self.memory = memory
         self.zoom = zoom
         self.number_of_pixels = number_of_pixels
         self.color = color
         self.photo_quality = photo_quality
-        # print("Конструктор")
+
 
     def __str__(self):
         return "Name: " + str(self.name) + "\nMemory: " + str(self.memory) + "GB" + "\nZoom: " + str(self.zoom) + "X" \
@@ -21,11 +20,16 @@ class Camera:
                + str(self.photo_quality)
 
     def __del__(self):
-        print('Деструктор')
+        pass
 
 
-out = Camera("Canon ", 128, 15, 12, "Black", "Good")
-print(out.__str__())
+def main():
+    canon = Camera("Canon", 128, 15, 12, "Black", "Good")
+    sony = Camera("SONY", 256, 18, 7, "Gold", "Good")
+    xioami = Camera("XIAOMI", 64, 13, 8, "Green", "BAD")
+
+    print(canon.__str__(),"\n","\n",sony.__str__(),"\n","\n",xioami.__str__())
 
 
-
+if __name__ == '__main__':
+    main()
