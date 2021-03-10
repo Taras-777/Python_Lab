b3599466ce1dc558885ca1cda7b04f02b9cf58c1
Taras-777 <taracxomun@gmail.com>
2021-03-10 17:52:27 +0200

    1

--- Lab1.py
+++ Lab1.py
@@ -5,7 +5,7 @@ class Camera:
     def static():
         return Camera.camera
 
-    def __init__(self, name ="", memory=0, zoom=0, number_of_pixels=0, color="", photo_quality=""):
+    def __init__(self, name="", memory=0, zoom=0, number_of_pixels=0, color="", photo_quality=""):
         self.name = name
         self.memory = memory
         self.zoom = zoom
@@ -13,10 +13,9 @@ class Camera:
         self.color = color
         self.photo_quality = photo_quality
 
-
     def __str__(self):
         return "Name: " + str(self.name) + "\nMemory: " + str(self.memory) + "GB" + "\nZoom: " + str(self.zoom) + "X" \
-          "\nNumber of pixels: " + str(self.number_of_pixels) + "\nColor: " + str(self.color) + "\nPhoto quality: " \
+            "\nNumber of pixels: " + str( self.number_of_pixels) + "\nColor: " + str(self.color) + "\nPhoto quality: " \
                + str(self.photo_quality)
 
     def __del__(self):
@@ -24,21 +23,12 @@ class Camera:
 
 
 def main():
-    Camera.camera
     canon = Camera("Canon", 128, 15, 12, "Black", "Good")
     sony = Camera("SONY", 256, 18, 7, "Gold", "Good")
     xioami = Camera("XIAOMI", 64, 13, 8, "Green", "BAD")
 
-    print(canon.__str__(),"\n","\n",sony.__str__(),"\n","\n",xioami.__str__())
-    print(Camera.camera)
-Camera.static()
+    print(canon.__str__(), "\n", "\n", sony.__str__(), "\n", "\n", xioami.__str__())
+
 
 if __name__ == '__main__':
     main()
-
-
-for i in range(101):
-    if i % 7 == 0:
-        print(i)
-
-
